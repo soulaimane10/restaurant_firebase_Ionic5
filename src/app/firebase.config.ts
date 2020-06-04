ab16a36ed3bd7aa6bb1f9602bb69015190746556
soulaimane10 <soulaimane861@gmail.com>
2020-06-04 19:30:04 +0200

    first commit

---
+++ src/app/firebase.config.ts
@@ -0,0 +1,21 @@
+//config default
+export const firebaseConfig = {
+  apiKey: "AIzaSyDoZWQ22kqyu5vu-5XaKWw45lYRk5Ya694",
+  authDomain: "rest-firebase-9d8e2.firebaseapp.com",
+  databaseURL: "https://rest-firebase-9d8e2.firebaseio.com",
+  projectId: "rest-firebase-9d8e2",
+  storageBucket: "rest-firebase-9d8e2.appspot.com",
+  messagingSenderId: "621461127050",
+  appId: "1:621461127050:web:d2ebe5581bda2ebf5d1273",
+  measurementId: "G-JV46PHZPGH"
+};
+
+// Sandip's firebase configuration
+// export const firebaseConfig = {
+//     apiKey: "AIzaSyAzusy10a0pSk-huxJO-DUtK0RoGgqJcpM",
+//     authDomain: "restaurantweb-ionic-app.firebaseapp.com",
+//     databaseURL: "https://restaurantweb-ionic-app.firebaseio.com",
+//     projectId: "restaurantweb-ionic-app",
+//     storageBucket: "restaurantweb-ionic-app.appspot.com",
+//     messagingSenderId: "120142074708"
+//   };
